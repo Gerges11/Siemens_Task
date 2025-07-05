427901223e347ae8e807c588b8b891d8b351441c
Gerges Samer <61360057+Gerges11@users.noreply.github.com>
2025-07-06 01:09:47 +0300

    Update nightwatch.conf.js

--- my-nightwatch-project/nightwatch.conf.js
+++ my-nightwatch-project/nightwatch.conf.js
@@ -1,6 +1,6 @@
 module.exports = {
   src_folders: ["tests"],
-  page_objects_path: ["nightwatch/page-objects"], // <-- ADD THIS
+  page_objects_path: ["nightwatch/page-objects"],
 
   webdriver: {
     start_process: true,
@@ -12,7 +12,17 @@ module.exports = {
     default: {
       launch_url: 'http://automationpractice.multiformis.com/index.php',
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: 'chrome',
+        chromeOptions: {
+          args: [
+            "--headless",
+            "--no-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu",
+            "--window-size=1920,1080",
+            `--user-data-dir=/tmp/chrome-user-data-${process.pid}`
+          ]
+        }
       }
     }
   }
