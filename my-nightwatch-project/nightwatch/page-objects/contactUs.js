251a2a1ff7f37021d140f4a77cec01ebd75af567
Gerges Samer <61360057+Gerges11@users.noreply.github.com>
2025-07-06 01:26:39 +0300

    Update contactUs.js

--- my-nightwatch-project/nightwatch/page-objects/contactUs.js
+++ my-nightwatch-project/nightwatch/page-objects/contactUs.js
@@ -1,14 +1,14 @@
 module.exports = {
-    url: 'http://automationpractice.multiformis.com/index.php?controller=contact',
-    elements: {
-      subject: '#id_contact',
-      email: '#email',
-      orderRef: '#id_order',
-      message: '#message',
-      fileUpload: '#fileUpload',
-      submitButton: '#submitMessage',
-      successAlert: '.alert-success',
-      errorAlert: '.alert-danger'
-    }
-  };
-  
+  url: 'http://automationpractice.multiformis.com/index.php?controller=contact',
+  elements: {
+    subject: '#id_contact',
+    email: '#email',
+    orderRef: '#id_order',
+    message: '#message',
+    // Note: on this demo site, the file input id is actually 'fileUpload'
+    fileUpload: '#fileUpload', 
+    submitButton: '#submitMessage', // id of the submit button
+    successAlert: '.alert-success',
+    errorAlert: '.alert-danger'
+  }
+};
