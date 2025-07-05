739740bdc9d335fb2f2750440677c75be3553e89
Gerges Samer <61360057+Gerges11@users.noreply.github.com>
2025-07-06 01:27:26 +0300

    Update homePage.js

--- my-nightwatch-project/nightwatch/page-objects/homePage.js
+++ my-nightwatch-project/nightwatch/page-objects/homePage.js
@@ -1,9 +1,8 @@
 module.exports = {
-    url: 'http://automationpractice.multiformis.com/index.php',
-    elements: {
-      searchBox: '#search_query_top',
-      searchButton: 'button[name="submit_search"]',
-      productList: '.product_list'
-    }
-  };
-  
+  url: 'http://automationpractice.multiformis.com/index.php',
+  elements: {
+    searchBox: '#search_query_top',             // input box
+    searchButton: 'button[name="submit_search"]', // submit button
+    productList: '.product_list'                 // container for products
+  }
+};
